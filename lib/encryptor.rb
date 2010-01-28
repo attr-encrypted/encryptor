d9eac7fa64382618ca6bdb2a52f0ffe2a7b3b818
Sean Huber <shuber@huberry.com>
2010-01-28 13:06:35 -0800

    extract from Huberry namespace and explicitly define default_options

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -1,51 +1,50 @@
 require 'openssl'
 
-module Huberry
-  module Encryptor
-    # The default options to use when calling the <tt>encrypt</tt> and <tt>decrypt</tt> methods
-    #
-    # Defaults to { :algorithm => 'aes-256-cbc' }
-    #
-    # Run 'openssl list-cipher-commands' in your terminal to view a list all cipher algorithms that are supported on your platform
-    class << self; attr_accessor :default_options; end
-    self.default_options = { :algorithm => 'aes-256-cbc' }
-    
-    # Encrypts a <tt>:value</tt> with a specified <tt>:key</tt>
-    #
-    # Optionally accepts <tt>:iv</tt> and <tt>:algorithm</tt> options
-    #
-    # Example
-    #
-    #   encrypted_value = Huberry::Encryptor.encrypt(:value => 'some string to encrypt', :key => 'some secret key')
-    def self.encrypt(options)
-      crypt :encrypt, options
-    end
-    
-    # Decrypts a <tt>:value</tt> with a specified <tt>:key</tt>
-    #
-    # Optionally accepts <tt>:iv</tt> and <tt>:algorithm</tt> options
-    #
-    # Example
-    #
-    #   decrypted_value = Huberry::Encryptor.decrypt(:value => 'some encrypted string', :key => 'some secret key')
-    def self.decrypt(options)
-      crypt :decrypt, options
-    end
-    
-    protected
-    
-      def self.crypt(cipher_method, options = {})
-        options = default_options.merge(options)
-        cipher = OpenSSL::Cipher::Cipher.new(options[:algorithm])
-        cipher.send(cipher_method)
-        if options[:iv]
-          cipher.key = options[:key]
-          cipher.iv = options[:iv]
-        else
-          cipher.pkcs5_keyivgen(options[:key])
-        end
-        result = cipher.update(options[:value])
-        result << cipher.final
-      end
+module Encryptor
+  # The default options to use when calling the <tt>encrypt</tt> and <tt>decrypt</tt> methods
+  #
+  # Defaults to { :algorithm => 'aes-256-cbc' }
+  #
+  # Run 'openssl list-cipher-commands' in your terminal to view a list all cipher algorithms that are supported on your platform
+  def self.default_options
+    @default_options ||= { :algorithm => 'aes-256-cbc' }
   end
+  
+  # Encrypts a <tt>:value</tt> with a specified <tt>:key</tt>
+  #
+  # Optionally accepts <tt>:iv</tt> and <tt>:algorithm</tt> options
+  #
+  # Example
+  #
+  #   encrypted_value = Huberry::Encryptor.encrypt(:value => 'some string to encrypt', :key => 'some secret key')
+  def self.encrypt(options)
+    crypt :encrypt, options
+  end
+  
+  # Decrypts a <tt>:value</tt> with a specified <tt>:key</tt>
+  #
+  # Optionally accepts <tt>:iv</tt> and <tt>:algorithm</tt> options
+  #
+  # Example
+  #
+  #   decrypted_value = Huberry::Encryptor.decrypt(:value => 'some encrypted string', :key => 'some secret key')
+  def self.decrypt(options)
+    crypt :decrypt, options
+  end
+  
+  protected
+  
+    def self.crypt(cipher_method, options = {})
+      options = default_options.merge(options)
+      cipher = OpenSSL::Cipher::Cipher.new(options[:algorithm])
+      cipher.send(cipher_method)
+      if options[:iv]
+        cipher.key = options[:key]
+        cipher.iv = options[:iv]
+      else
+        cipher.pkcs5_keyivgen(options[:key])
+      end
+      result = cipher.update(options[:value])
+      result << cipher.final
+    end
 end
--- test/encryptor_test.rb
+++ test/encryptor_test.rb
@@ -11,33 +11,33 @@ class EncryptorTest < Test::Unit::TestCase
   
   algorithms.each do |algorithm|
     define_method "test_should_crypt_with_#{algorithm}_algorithm_with_iv" do
-      encrypted_value = Huberry::Encryptor.encrypt(:value => original_value, :key => key, :iv => iv, :algorithm => algorithm)
+      encrypted_value = Encryptor.encrypt(:value => original_value, :key => key, :iv => iv, :algorithm => algorithm)
       assert_not_equal original_value, encrypted_value
-      assert_not_equal Huberry::Encryptor.encrypt(:value => original_value, :key => key, :algorithm => algorithm), encrypted_value
-      assert_equal original_value, Huberry::Encryptor.decrypt(:value => encrypted_value, :key => key, :iv => iv, :algorithm => algorithm)
+      assert_not_equal Encryptor.encrypt(:value => original_value, :key => key, :algorithm => algorithm), encrypted_value
+      assert_equal original_value, Encryptor.decrypt(:value => encrypted_value, :key => key, :iv => iv, :algorithm => algorithm)
     end
     
     define_method "test_should_crypt_with_#{algorithm}_algorithm_without_iv" do
-      encrypted_value = Huberry::Encryptor.encrypt(:value => original_value, :key => key, :algorithm => algorithm)
+      encrypted_value = Encryptor.encrypt(:value => original_value, :key => key, :algorithm => algorithm)
       assert_not_equal original_value, encrypted_value
-      assert_equal original_value, Huberry::Encryptor.decrypt(:value => encrypted_value, :key => key, :algorithm => algorithm)
+      assert_equal original_value, Encryptor.decrypt(:value => encrypted_value, :key => key, :algorithm => algorithm)
     end
   end
   
   define_method 'test_should_have_a_default_algorithm' do
-    assert algorithms.include?(Huberry::Encryptor.default_options[:algorithm])
+    assert algorithms.include?(Encryptor.default_options[:algorithm])
   end
   
   define_method 'test_should_use_the_default_algorithm_if_one_is_not_specified' do
-    assert_equal Huberry::Encryptor.encrypt(:value => original_value, :key => key, :algorithm => Huberry::Encryptor.default_options[:algorithm]), Huberry::Encryptor.encrypt(:value => original_value, :key => key)
+    assert_equal Encryptor.encrypt(:value => original_value, :key => key, :algorithm => Encryptor.default_options[:algorithm]), Encryptor.encrypt(:value => original_value, :key => key)
   end
   
   def test_should_be_able_to_change_the_default_algorithm
-    original_algorithm = Huberry::Encryptor.default_options[:algorithm]
+    original_algorithm = Encryptor.default_options[:algorithm]
     assert_not_equal 'test', original_algorithm
-    Huberry::Encryptor.default_options[:algorithm] = 'test'
-    assert_equal 'test', Huberry::Encryptor.default_options[:algorithm]
-    Huberry::Encryptor.default_options[:algorithm] = original_algorithm
+    Encryptor.default_options[:algorithm] = 'test'
+    assert_equal 'test', Encryptor.default_options[:algorithm]
+    Encryptor.default_options[:algorithm] = original_algorithm
   end
   
 end
