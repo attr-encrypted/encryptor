b56b0f64a52110eabf5298e65e216f04b7196f0a
Sean Huber <sean@lendinghome.com>
2015-09-12 14:27:39 -0700

    Fix "constant redefinition" warnings when running specs
    * test/openssl_helper.rb:19 warning: already initialized constant OpenSSLHelper::ALGORITHMS
    * test/openssl_helper.rb:4  warning: previous definition of ALGORITHMS was here

--- test/openssl_helper.rb
+++ test/openssl_helper.rb
@@ -2,7 +2,7 @@ module OpenSSLHelper
   # For test debugging
   puts "OpenSSL Version: #{OpenSSL::OPENSSL_VERSION}"
 
-  ALGORITHMS = OpenSSL::Cipher.ciphers
+  algorithms = OpenSSL::Cipher.ciphers
 
   case RUBY_PLATFORM.to_sym
   when :java
@@ -15,12 +15,12 @@ module OpenSSLHelper
     # OpenSSL::Cipher::CipherError: key length too short
     # if key length is 24 bytes or more:
     # OpenSSL::Cipher::CipherError: DES key too long - should be 8 bytes: possibly you need to install Java Cryptography Extension (JCE) Unlimited Strength Jurisdiction Policy Files for your JRE
-    ALGORITHMS -= %w(des-ede3 DES-EDE3)
+    algorithms -= %w(des-ede3 DES-EDE3)
   else
-    ALGORITHMS &= %x(openssl list-cipher-commands).split
+    algorithms &= %x(openssl list-cipher-commands).split
   end
 
-  ALGORITHMS.freeze
 
+  ALGORITHMS = algorithms.freeze
   AUTHENTICATED_ENCRYPTION_ALGORITHMS = ['aes-128-gcm','aes-192-gcm','aes-256-gcm']
 end
