ef0da4ce44b665ed94d6fd2b84336f1c97a12dc6
Sean Huber <sean@lendinghome.com>
2015-09-12 14:33:26 -0700

    Reformat comment in test/openssl_helper for clarity

--- test/openssl_helper.rb
+++ test/openssl_helper.rb
@@ -10,10 +10,13 @@ module OpenSSLHelper
     restricted_field.accessible = true
     restricted_field.set(nil, false)
 
-    # if key length is less than 24 bytes:
-    # OpenSSL::Cipher::CipherError: key length too short
-    # if key length is 24 bytes or more:
-    # OpenSSL::Cipher::CipherError: DES key too long - should be 8 bytes: possibly you need to install Java Cryptography Extension (JCE) Unlimited Strength Jurisdiction Policy Files for your JRE
+    # If key length is less than 24 bytes:
+    #   OpenSSL::Cipher::CipherError: key length too short
+    #
+    # If key length is 24 bytes or more:
+    #   OpenSSL::Cipher::CipherError: DES key too long - should be 8 bytes:
+    #     possibly you need to install Java Cryptography Extension (JCE)
+    #     Unlimited Strength Jurisdiction Policy Files for your JRE
     algorithms -= %w(des-ede3 DES-EDE3)
   else
     algorithms &= %x(openssl list-cipher-commands).split
